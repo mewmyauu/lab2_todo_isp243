e27e0620f26a192274796aa1e8f96160a0cc3a93
mewmyauu <rk.vladislav@gmail.com>
2026-09-14 15:43:51 +0300

    initial dart project

--- bin/lab2_todo_isp243.dart
+++ bin/lab2_todo_isp243.dart
@@ -1,5 +1,3 @@
-import 'package:lab2_todo_isp243/lab2_todo_isp243.dart' as lab2_todo_isp243;
-
-void main(List<String> arguments) {
-  print('Hello world: ${lab2_todo_isp243.calculate()}!');
+void main() {
+  
 }
